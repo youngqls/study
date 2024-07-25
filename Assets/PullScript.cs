69a62b1f8f2430c718caf5c90ad24d89fbb6f706
youngqls <wndudqls21@gmail.com>
2024-07-26 02:10:33 +0900

    머지 테스트

--- Assets/PullScript.cs
+++ Assets/PullScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PullScript : MonoBehaviour
 {
+    public Vector3 position;
     public bool apple = true;
     // Start is called before the first frame update
     void Start()
