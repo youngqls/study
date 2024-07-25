0dbd84579ac86171389e039abae8ed360f78ed06
youngqls <127822800+youngqls@users.noreply.github.com>
2024-07-26 02:25:28 +0900

    Update PullScript.cs

--- Assets/PullScript.cs
+++ Assets/PullScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PullScript : MonoBehaviour
 {
     public Transform transform;
+    private bool apple = true;
    
     // Start is called before the first frame update
     void Start()
