e5288ac15717ace7418e07a040b6f758f8e37764
youngqls <127822800+youngqls@users.noreply.github.com>
2024-07-26 02:10:17 +0900

    머지 텟스트

--- PullScript.cs
+++ PullScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PullScript : MonoBehaviour
 {
+public Transform transform;
     public float apple = 100f;
     // Start is called before the first frame update
     void Start()
